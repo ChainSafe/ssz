eccc8bcc7e7a41b0d4e160efb06cba2798e1bcd6
Cayman <caymannava@gmail.com>
2019-06-08 19:35:07 -0500

    Tweaks

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,8 @@ export default function() {
   return (
     <div className='section'>
       <div className='container'>
-        <h1 className='title is-family-code'>Simple Serialize<span className='is-size-6'> the encoding for Ethereum2.0</span></h1>
-        <p> This website uses a fully functional and spec compliant version SSZ written in Typescript specifically for the browser, offering a glimpse of what can be acheived in the web for Ethereum2.0. The specification can be found <a target="_blank" href="https://github.com/ethereum/eth2.0-specs/blob/dev/specs/simple-serialize.md">here.</a></p>
+        <h1 className='title is-family-code'>Simple Serialize<span className='is-size-6'> encoding for Ethereum2.0</span></h1>
+        <p>This is a spec compliant version SSZ written in Typescript specifically for the browser, offering a glimpse of what can be acheived in the web for Ethereum2.0. Find the specification <a target="_blank" href="https://github.com/ethereum/eth2.0-specs/blob/dev/specs/simple-serialize.md">here.</a></p>
       </div>
     </div>
   );
