f70261c63b77a58c2f72726ff2a5f80b1acb9b9c
Gregory Markou <gregorymarkou@gmail.com>
2019-06-10 09:12:35 -0400

    fix sentance structure

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function() {
     <div className='section'>
       <div className='container'>
         <h1 className='title is-family-code'>Simple Serialize<span className='is-size-6'> encoding for Ethereum2.0</span></h1>
-        <p>This is a spec compliant version SSZ written in Typescript specifically for the browser, offering a glimpse of what can be acheived in the web for Ethereum2.0. Find the specification <a target="_blank" href="https://github.com/ethereum/eth2.0-specs/blob/dev/specs/simple-serialize.md">here.</a></p>
+        <p>This is a browser ready version of SSZ written in Typescript, offering a glimpse of what can be acheived in the web for Ethereum2.0. Find the specification <a target="_blank" href="https://github.com/ethereum/eth2.0-specs/blob/dev/specs/simple-serialize.md">here.</a></p>
       </div>
     </div>
   );
