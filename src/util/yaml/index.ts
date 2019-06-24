f232ea87307fe2135a228a4239b8c819141d9899
protolambda <proto@protolambda.com>
2019-06-24 20:45:33 +0200

    make json handle uint64 as string, fix yaml

--- src/util/yaml/index.ts
+++ src/util/yaml/index.ts
@@ -1,9 +1,11 @@
+// @ts-ignore
 import yaml from 'js-yaml';
 
+// @ts-ignore
 import * as ETH_SCHEMA from './schema';
 
 export function dumpYaml(input: any): string {
-  return yaml.dump(input);
+  return yaml.dump(input, {schema: ETH_SCHEMA});
 }
 
 export function parseYaml(input: string): any {
