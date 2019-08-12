d016af440894a54e40b2f0b1994dcafaac63a1bc
Eric Tu <tu.eric@hotmail.com>
2019-08-12 18:02:53 -0400

    add a wrapper function hashBrowns

--- assembly/index.ts
+++ assembly/index.ts
@@ -25,6 +25,19 @@ export class  jsSHA256{
       this.state[6] = 0x1f83d9ab;
       this.state[7] = 0x5be0cd19;
     }
+
+    public init(): void{
+      this.datalen = 0;
+      this.bitlen = 0;
+      this.state[0] = 0x6a09e667;
+      this.state[1] = 0xbb67ae85;
+      this.state[2] = 0x3c6ef372;
+      this.state[3] = 0xa54ff53a;
+      this.state[4] = 0x510e527f;
+      this.state[5] = 0x9b05688c;
+      this.state[6] = 0x1f83d9ab;
+      this.state[7] = 0x5be0cd19; 
+    }
   
     private k: Word[] = [
       0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
@@ -150,11 +163,8 @@ export class  jsSHA256{
         while (i < 64)
           this.data[i++] = 0x00;
         this.sha256_transform();
-        // TODO: WHY DID I COMMENT THIS OUT
-        //memset(this.data, 0, 56);
-        for (let x = 0; x < 56; x++) {
-          this.data[x] = 0;
-        }
+d
+        this.data.fill(0, 0, 56);
       }
 
       this.bitlen += this.datalen * 8;
@@ -183,6 +193,12 @@ export class  jsSHA256{
     }
     
     public digest(): Uint8Array{
+      return this.hash;
+    }
+
+    public hashBrowns(data: Uint8Array, len: u32): Uint8Array{
+      this.sha256_update(data, len);
+      this.sha256_final()
       return this.hash;
     }
   }
