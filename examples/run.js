905a358779c1a32b4c8e0f5bdb0f341c0fa1326d
Gregory Markou <gregorymarkou@gmail.com>
2019-09-03 19:47:33 -0400

    browser stuff

--- examples/run.js
+++ examples/run.js
@@ -1,30 +1,8 @@
 
 const crypto = require('crypto');
-const sha256 = require('fast-sha256');
 const wasm = require('../wasm.js');
 
-const f = new wasm.tsSHA256Fast()
-// sha256("abc")
 
-// f.sha256_update(wasm.newArray(new Uint8Array([97, 98, 99])), 3)
-// f.sha256_final();
-f.update(wasm.newArray(new Uint8Array([97, 98, 99])), 3);
-let arr = wasm.getArray(Uint8Array, f.digest());
-
-console.log(arr.length)
-console.log(`tsSHA256Fast `, toHexString(arr))
-
-f.clean()
-f.update(wasm.newArray(new Uint8Array([97, 98, 99])), 3);
-let res = wasm.getArray(Uint8Array, f.digest());
-console.log(`tsSHA256Fast again `, toHexString(res))
-
-
-function jsHash(msg) {
-  const hasher = new sha256.Hash();
-  hasher.update(msg);
-  return hasher.digest();
-}
 
 const toHexString = byteArray => byteArray.reduce((acc, val) => (acc + ('0' + val.toString(16)).slice(-2)), '');
 
@@ -49,32 +27,17 @@ const randomessage16384 = wasm.__retain(wasm.__allocArray(wasm.UINT8ARRAY_ID, ra
 
 console.time('as (wasm)');
 
-  const messageOut = wasm.hashMe(message);
-  const amessageOut = wasm.hashMe(amessage);
-  const emptymessageOut = wasm.hashMe(emptymessage);
+  const messageOut = wasm.hash(message);
+  const amessageOut = wasm.hash(amessage);
+  const emptymessageOut = wasm.hash(emptymessage);
   for (let i = 0; i < 1000; i++) {
-    wasm.hashMe(randomessage2048);
+    wasm.hash(randomessage2048);
   }
   for (let i = 0; i < 100; i++) {
-    wasm.hashMe(randomessage16384);
+    wasm.hash(randomessage16384);
   }
 
 console.timeEnd('as (wasm)');
-
-console.time('js (fast-sha256)');
-
-  const js_messageOut = jsHash(Message);
-  const js_amessageOut = jsHash(aMessage);
-  const js_emptymessageOut = jsHash(emptyMessage);
-  for (let i = 0; i < 1000; i++) {
-    jsHash(randomMessage2048);
-  }
-  for (let i = 0; i < 100; i++) {
-    jsHash(randomMessage16384);
-  }
-
-console.timeEnd('js (fast-sha256)');
-
 console.log(toHexString(wasm.__getArray(messageOut)), '7321348c8894678447b54c888fdbc4e4b825bf4d1eb0cfb27874286a23ea9fd2');
 console.log(toHexString(wasm.__getArray(amessageOut)), 'ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad');
 console.log(toHexString(wasm.__getArray(emptymessageOut)), 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855');
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'externals': {
+    'electron': "require('electron')",
+    'child_process': "require('child_process')",
+    'fs': "require('fs')",
+    'path': "require('path')"
+  }
+}
