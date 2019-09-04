1673807ee26c0a50999bfb32950b9bb43ea9134d
Cayman <caymannava@gmail.com>
2019-09-04 15:23:09 -0500

    Clean up types

--- src/util/types.ts
+++ src/util/types.ts
@@ -2,23 +2,5 @@ import {AnySSZType} from "@chainsafe/ssz";
 
 import {types as rawTypes} from '../types';
 
-
-function _typeNames(types: any): string[] {
-  // const names = [];
-  // for (const name in types) {
-  //   names.push(name);
-  // }
-  return Object.keys(rawTypes)
-}
-
-
-function _typeTypes(names: string[]): Record<string, AnySSZType> {
-  // const types: Record<string, AnySSZType> = {};
-  // for (const name of names) {
-  //   types[name] = (rawTypes as Record<string, AnySSZType>)[name];
-  // }
-  return rawTypes as unknown as Record<string, AnySSZType>;
-}
-
-export const names = _typeNames(rawTypes).sort();
-export const types = _typeTypes(names);
+export const names = Object.keys(rawTypes).sort();
+export const types = rawTypes as unknown as Record<string, AnySSZType>;
