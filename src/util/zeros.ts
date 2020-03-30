27db731e5103800b39fb5514df48537e7fe679f1
Cayman <caymannava@gmail.com>
2020-03-29 21:29:57 -0500

    Fix type errors

--- src/util/zeros.ts
+++ src/util/zeros.ts
@@ -5,5 +5,5 @@ import {hash} from "./hash";
 // create array of "zero hashes", successively hashed zero chunks
 export const zeroHashes = [Buffer.alloc(BYTES_PER_CHUNK)];
 for (let i = 0; i < 52; i++) {
-  zeroHashes.push(hash(zeroHashes[i], zeroHashes[i]));
+  zeroHashes.push(Buffer.from(hash(zeroHashes[i], zeroHashes[i])));
 }
