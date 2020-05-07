6a4b08ae01ab1fef2a33e050f31164179818f0fb
Cayman <caymannava@gmail.com>
2020-05-07 15:47:08 -0500

    Copy edits

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,16 +11,18 @@ export default function() {
         and <a className='is-link has-text-danger is-family-code' href='https://github.com/ChainSafe/simpleserialize.com/graphs/contributors'>ETH 2.0 friends</a>
       </div>
       <div className='content has-text-centered is-small is-family-code'>
-        <a className='is-link has-text-grey'
-          href='https://www.npmjs.com/package/@chainsafe/ssz'>
-          @chainsafe/ssz {pkg.dependencies['@chainsafe/ssz']}
-        </a>
-      </div>
-      <div className='content has-text-centered is-small is-family-code'>
-        <a className='is-link has-text-grey'
-          href='https://www.npmjs.com/package/@chainsafe/lodestar-types'>
-          @chainsafe/lodestar-types {pkg.dependencies['@chainsafe/lodestar-types']}
-        </a>
+        <div>
+          <a className='is-link has-text-grey'
+            href='https://www.npmjs.com/package/@chainsafe/ssz'>
+            @chainsafe/ssz {pkg.dependencies['@chainsafe/ssz']}
+          </a>
+        </div>
+        <div>
+          <a className='is-link has-text-grey'
+            href='https://www.npmjs.com/package/@chainsafe/lodestar-types'>
+            @chainsafe/lodestar-types {pkg.dependencies['@chainsafe/lodestar-types']}
+          </a>
+        </div>
       </div>
     </footer>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,8 @@ export default function() {
   return (
     <div className='section'>
       <div className='container'>
-        <h1 className='title is-family-code'>Simple Serialize<span className='is-size-6'> encoding for Ethereum2.0</span></h1>
-        <p>This is a browser ready version of SSZ written in Typescript, offering a glimpse of what can be acheived in the web for Ethereum2.0. Find the specification <a target="_blank" href="https://github.com/ethereum/eth2.0-specs/blob/dev/specs/simple-serialize.md">here.</a></p>
+        <h1 className='title is-family-code'>Simple Serialize <span className='is-size-6'><img src="https://img.shields.io/badge/ETH2_Spec_Version-0.11.2-2e86c1.svg"/></span></h1>
+        <p>Simple Serialize (SSZ) is a serialization and merkleization standard created specifically for Eth2. Find the specification <a target="_blank" href="https://github.com/ethereum/eth2.0-specs/blob/dev/specs/simple-serialize.md">here.</a></p>
       </div>
     </div>
   );
