5e7d23463819be516012368cd0bb3d1b2611f089
Marin Petrunić <mpetrunic@users.noreply.github.com>
2020-06-05 10:07:50 +0200

    Bind spec url to spec tag

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export default function(): JSX.Element {
             <img src="https://img.shields.io/badge/ETH2_Spec_Version-0.11.2-2e86c1.svg"/>
           </span>
         </h1>
-        <p>Simple Serialize (SSZ) is a serialization and merkleization standard created specifically for Eth2. Find the specification <a target="_blank" href="https://github.com/ethereum/eth2.0-specs/blob/dev/specs/simple-serialize.md">here.</a></p>
+        <p>Simple Serialize (SSZ) is a serialization and merkleization standard created specifically for Eth2. Find the specification <a target="_blank" href="https://github.com/ethereum/eth2.0-specs/blob/v0.11.2/ssz/simple-serialize.md">here.</a></p>
       </div>
     </div>
   );
