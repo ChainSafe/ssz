761653cb2b72c908b8a681b87203c5e08a7dd036
Cayman <caymannava@gmail.com>
2021-03-22 20:44:51 -0500

    Combine tree backing files, add todo

--- src/backings/tree/index.ts
+++ src/backings/tree/index.ts
@@ -1,6 +1,2 @@
 export * from "./interface";
-export * from "./abstract";
-export * from "./array";
-export * from "./list";
-export * from "./container";
-export * from "./proxy";
+export * from "./treeValue";
