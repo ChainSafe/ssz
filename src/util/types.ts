bc9f9b8f1d244f5dfa8fc1f0fdceed25f16e364d
3xtr4t3rr3str14l <k0m0d0tr0n@protonmail.com>
2021-07-01 15:22:44 -0500

    PR feedback

--- src/util/types.ts
+++ src/util/types.ts
@@ -1,11 +1,11 @@
 import {Type} from "@chainsafe/ssz";
-import * as sszTypes from "@chainsafe/lodestar-types/lib/sszTypes";
+import {ssz} from "@chainsafe/lodestar-types";
 
-const {phase0, altair, allForks, ...primitive} = sszTypes;
+const {phase0, altair, allForks, ...primitive} = ssz;
 
 export const forks = {
   phase0: {...phase0, ...primitive} as Record<string, Type<unknown>>,
-  altair: {...altair, ...primitive} as Record<string, Type<unknown>>,
+  altair: {...phase0, ...altair, ...primitive} as Record<string, Type<unknown>>,
 }
 
 export type ForkName = keyof typeof forks;
