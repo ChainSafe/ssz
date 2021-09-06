23471147a90db0e8c68b78d770eff99194bfeccd
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2021-09-06 18:23:17 +0200

    Update json.ts

--- packages/ssz/src/util/json.ts
+++ packages/ssz/src/util/json.ts
@@ -2,5 +2,5 @@ import {IJsonOptions} from "../types";
 import Case from "case";
 
 export function toExpectedCase(value: string, expectedCase?: IJsonOptions["case"]): string {
-  return expectedCase && expectedCase != "notransform" && Case[expectedCase] ? Case[expectedCase](value) : value;
+  return expectedCase === "notransform" ? value : Case[expectedCase](value);
 }
