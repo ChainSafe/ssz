82864548f30694170aa11a2dd27fe90e635c9eda
dapplion <35266934+dapplion@users.noreply.github.com>
2021-12-25 20:55:50 +0100

    Run spec generic tests for v2 types

--- packages/ssz/test/spec/generic/index.test.ts
+++ packages/ssz/test/spec/generic/index.test.ts
@@ -4,6 +4,7 @@ import fs from "fs";
 // eslint-disable-next-line no-restricted-imports
 import {parseInvalidTestcase, parseValidTestcase} from "@chainsafe/lodestar-spec-test-util/lib/sszGeneric";
 import {CompositeType, isCompositeType, toHexString, Type} from "../../../src";
+import {Type as TypeV2, CompositeType as CompositeTypeV2} from "../../../src/v2/abstract";
 import {SPEC_TEST_LOCATION} from "../../specTestVersioning";
 
 // Test types defined here
@@ -20,9 +21,9 @@ type TypeVN = {
 };
 type GetTestType = (testType: string, testCase: string) => TypeVN;
 
-const byTypeVersion: [GetTestType, string][] = [
-  [getTestTypeV1, "v1"],
-  [getTestTypeV2, "v2"],
+const byTypeVersion: [GetTestType, "v1" | "v2"][] = [
+  [getTestTypeV1, "v1" as const],
+  [getTestTypeV2, "v2" as const],
 ];
 
 // ssz_generic
@@ -46,8 +47,19 @@ for (const testType of fs.readdirSync(rootGenericSszPath)) {
     for (const invalidCase of fs.readdirSync(invalidCasesPath)) {
       for (const [getTestType, typeV] of byTypeVersion) {
         it(`${invalidCase} - ${typeV}`, () => {
+          // TODO: Strong type errors and assert that the entire it() throws known errors
+          if (invalidCase.endsWith("_0") && typeV === "v2") {
+            expect(() => getTestType(testType, invalidCase), "Must throw constructing type").to.throw();
+            return;
+          }
+
           const type = getTestType(testType, invalidCase);
           const testData = parseInvalidTestcase(path.join(invalidCasesPath, invalidCase));
+
+          /* eslint-disable no-console */
+          if (process.env.DEBUG) {
+            console.log({serialized: Buffer.from(testData.serialized).toString("hex")});
+          }
 
           // Unlike the valid suite, invalid encodings do not have any value or hash tree root. The serialized data
           // should simply not be decoded without raising an error.
@@ -77,8 +89,12 @@ for (const testType of fs.readdirSync(rootGenericSszPath)) {
           const testDataSerialized = toHexString(testData.serialized);
           const testDataRoot = toHexString(testData.root);
 
+          if (process.env.DEBUG) {
+            console.log("serialized", Buffer.from(testData.serialized).toString("hex"));
+          }
+
           const serialized = wrapErr(() => type.serialize(testData.value), "type.serialize()");
-          const value = wrapErr(() => type.deserialize(testData.serialized), "type.deserialize()");
+          const value = wrapErr(() => type.deserialize(copy(testData.serialized)), "type.deserialize()");
           const root = wrapErr(() => type.hashTreeRoot(testData.value), "type.hashTreeRoot()");
           const valueSerdes = wrapErr(() => type.deserialize(serialized), "type.deserialize(serialized)");
 
@@ -88,26 +104,47 @@ for (const testType of fs.readdirSync(rootGenericSszPath)) {
           expect(toHexString(root)).to.equal(testDataRoot, "struct hashTreeRoot");
 
           // If the type is composite, test tree-backed ops
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          if (!isCompositeType(type as Type<any>)) return;
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          const compositeType = type as CompositeType<any>;
+          if (typeV === "v1") {
+            if (!isCompositeType(type as Type<unknown>)) return;
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            const compositeType = type as CompositeType<any>;
 
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-          const treebackedValue = compositeType.createTreeBackedFromStruct(testData.value);
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-          const treeToStruct = compositeType.tree_convertToStruct(treebackedValue.tree);
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            const treebackedValue = compositeType.createTreeBackedFromStruct(testData.value);
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+            const treeToStruct = compositeType.tree_convertToStruct(treebackedValue.tree);
 
-          expect(treeToStruct).to.deep.equal(testData.value, "tree-backed to struct");
-          expect(type.equals(testData.value, treebackedValue), "struct - tree-backed type.equals()").to.be.true;
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-          expect(toHexString(treebackedValue.serialize())).to.equal(testDataSerialized, "tree-backed serialize");
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-          expect(toHexString(treebackedValue.hashTreeRoot())).to.equal(testDataRoot, "tree-backed hashTreeRoot");
+            expect(treeToStruct).to.deep.equal(testData.value, "tree-backed to struct");
+            expect(type.equals(testData.value, treebackedValue), "struct - tree-backed type.equals()").to.be.true;
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            expect(toHexString(treebackedValue.serialize())).to.equal(testDataSerialized, "tree-backed serialize");
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            expect(toHexString(treebackedValue.hashTreeRoot())).to.equal(testDataRoot, "tree-backed hashTreeRoot");
+          }
+
+          // v2
+          else if (typeV === "v2") {
+            if ((type as TypeV2<unknown>).isBasic) return;
+
+            const compositeType = type as CompositeTypeV2<any>;
+
+            const node = compositeType.tree_deserializeFromBytes(testData.serialized, 0, testData.serialized.length);
+            expect(toHexString(node.root)).to.equal(testDataRoot, "tree-backed hashTreeRoot");
+
+            const treeBytes = new Uint8Array(compositeType.tree_serializedSize(node));
+            compositeType.tree_serializeToBytes(treeBytes, 0, node);
+            expect(toHexString(treeBytes)).to.equal(testDataSerialized, "tree-backed serialize");
+          }
         });
       }
     }
   });
+}
+
+function copy(buf: Uint8Array): Uint8Array {
+  const copy = new Uint8Array(buf.length);
+  copy.set(buf);
+  return copy;
 }
 
 function wrapErr<T>(fn: () => T, prefix: string): T {
