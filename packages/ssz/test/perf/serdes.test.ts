bd8d4305024c1f594daa56771cbbb9f5b98fce64
dapplion <35266934+dapplion@users.noreply.github.com>
2022-01-14 19:39:44 +0100

    Fix benchmark type issues

--- packages/ssz/test/perf/serdes.test.ts
+++ packages/ssz/test/perf/serdes.test.ts
@@ -8,44 +8,31 @@ import {
   ValueOf,
   TreeView,
   TreeViewDU,
+  BitArray,
 } from "../../src";
 
 describe("SSZ (de)serialize", () => {
   const Gwei = new UintBigintType(8);
 
-  type TestCase<T extends CompositeType<unknown, unknown, unknown>> = {
-    id: string;
-    type: T;
-    getValue?: () => ValueOf<T>;
-  };
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  runTestCase("Simple object", new ContainerType({a: Gwei, b: Gwei, c: Gwei}));
+  runTestCase(
+    "aggregationBits",
+    new BitListType(2048),
+    () => new BitArray(new Uint8Array(Buffer.alloc(128 / 8, 0xff)), 128)
+  );
 
-  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-  function getBigListTestCase(arrLen: number) {
-    return {
-      id: `List(BigInt) ${arrLen}`,
-      type: new ListBasicType(Gwei, arrLen),
-      getValue: () => Array.from({length: arrLen}, () => BigInt(31217089836)),
-    };
+  for (const arrLen of [1e3, 1e4, 1e5]) {
+    runTestCase(`List(BigInt) ${arrLen}`, new ListBasicType(Gwei, arrLen), () =>
+      Array.from({length: arrLen}, () => BigInt(31217089836))
+    );
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const testCases: TestCase<CompositeType<unknown, TreeView<any>, TreeViewDU<any>>>[] = [
-    {
-      id: "Simple object",
-      type: new ContainerType({a: Gwei, b: Gwei, c: Gwei}),
-    },
-    {
-      id: "aggregationBits",
-      type: new BitListType(2048),
-      getValue: () => Array.from({length: 128}, () => true),
-    },
-
-    getBigListTestCase(1e3),
-    getBigListTestCase(1e4),
-    getBigListTestCase(1e5),
-  ];
-
-  for (const {id, type, getValue} of testCases) {
+  function runTestCase<T extends CompositeType<unknown, TreeView<any>, TreeViewDU<any>>>(
+    id: string,
+    type: T,
+    getValue?: () => ValueOf<T>
+  ): void {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const struct = getValue ? getValue() : type.defaultValue;
     const bytes = type.serialize(struct);
