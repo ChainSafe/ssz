c7d375354ffcd8a5b87b55ca5386874c6c3287d8
dapplion <35266934+dapplion@users.noreply.github.com>
2022-01-14 20:47:11 +0100

    Re-add UintBigint optimization

--- packages/ssz/src/type/uint.ts
+++ packages/ssz/src/type/uint.ts
@@ -4,6 +4,7 @@ import {BasicType} from "./abstract";
 /* eslint-disable @typescript-eslint/member-ordering */
 
 const MAX_SAFE_INTEGER_BN = BigInt(Number.MAX_SAFE_INTEGER);
+const BIGINT_4_BYTES = BigInt(32);
 
 export class UintNumberType extends BasicType<number> {
   readonly typeName: string;
@@ -182,23 +183,58 @@ export class UintBigintType extends BasicType<bigint> {
       throw Error(`Invalid size ${size} expected ${this.byteLength}`);
     }
 
+    // Motivation:
+    //   Creating BigInts and bitshifting is more expensive than
+    // number bitshifting.
+    // Implementation:
+    //   Iterate throuth the bytearray, bitshifting the data into a 'groupOutput' number, byte by byte
+    // After each 4 bytes, bitshift the groupOutput into the bigint output and clear the groupOutput out
+    // After iterating through the bytearray,
+    // There may be additional data in the groupOutput if the bytearray if the bytearray isn't divisible by 4
     let output = BigInt(0);
+    let groupIndex = 0,
+      groupOutput = 0;
     for (let i = 0; i < this.byteLength; i++) {
-      output += BigInt(data[start + i] * 2 ** (8 * i));
+      groupOutput += data[start + i] << (8 * (i % 4));
+      if ((i + 1) % 4 === 0) {
+        // Left shift returns a signed integer and the output may have become negative
+        // In that case, the output needs to be converted to unsigned integer
+        if (groupOutput < 0) {
+          groupOutput >>>= 0;
+        }
+        // Optimization to set the output the first time, forgoing BigInt addition
+        if (groupIndex === 0) {
+          output = BigInt(groupOutput);
+        } else {
+          output += BigInt(groupOutput) << BigInt(32 * groupIndex);
+        }
+        groupIndex++;
+        groupOutput = 0;
+      }
     }
-
+    // if this.byteLength isn't a multiple of 4, there will be additional data
+    if (groupOutput) {
+      output += BigInt(groupOutput >>> 0) << BigInt(32 * groupIndex);
+    }
     return output;
   }
 
   value_serializeToBytes(output: Uint8Array, offset: number, value: bigint): number {
+    // Motivation
+    // BigInt bit shifting and BigInt allocation is slower compared to number
+    // For every 4 bytes, we extract value to groupedBytes
+    // and do bit shifting on the number
     let v = value;
-    // TODO: Not-optimized, copy pasted from UintNumberType
-    const MAX_BYTE = BigInt(0xff);
+    let groupedBytes = Number(BigInt.asUintN(32, v));
     for (let i = 0; i < this.byteLength; i++) {
-      output[offset + i] = Number(v & MAX_BYTE);
-      v = v / BigInt(256);
+      output[offset + i] = Number(groupedBytes & 0xff);
+      if ((i + 1) % 4 !== 0) {
+        groupedBytes >>= 8;
+      } else {
+        v >>= BIGINT_4_BYTES;
+        groupedBytes = Number(BigInt.asUintN(32, v));
+      }
     }
-
     return offset + this.byteLength;
   }
 
