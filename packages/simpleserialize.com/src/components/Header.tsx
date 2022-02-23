e2d23d9a9dd57cc68bc80ad368d4a3501bafd04b
Cayman <caymannava@gmail.com>
2022-02-23 11:11:45 -0600

    Update simpleserialize.com w bellatrix types

--- packages/simpleserialize.com/src/components/Header.tsx
+++ packages/simpleserialize.com/src/components/Header.tsx
@@ -2,6 +2,8 @@
 
 import * as React from "react";
 
+const SPEC_VERSION = "1.1.9";
+
 export default function Header(): JSX.Element {
   return (
     <div className="section">
@@ -9,16 +11,16 @@ export default function Header(): JSX.Element {
         <h1 className="title is-family-code">
           Simple Serialize
           <span className="is-size-6">
-            <img src="https://img.shields.io/badge/ETH2_Spec_Version-0.11.2-2e86c1.svg" />
+            <img src={`https://img.shields.io/badge/Consensus_Spec_Version-${SPEC_VERSION}-2e86c1.svg`} />
           </span>
         </h1>
         <p>
-          Simple Serialize (SSZ) is a serialization and merkleization standard created specifically for Eth2. Find the
-          specification{" "}
+          Simple Serialize (SSZ) is a serialization and merkleization standard created specifically for Ethereum
+          consensus. Find the specification{" "}
           <a
             target="_blank"
             rel="noreferrer"
-            href="https://github.com/ethereum/eth2.0-specs/blob/v0.11.2/ssz/simple-serialize.md"
+            href={`https://github.com/ethereum/consensus-specs/blob/v${SPEC_VERSION}/ssz/simple-serialize.md`}
           >
             here.
           </a>
