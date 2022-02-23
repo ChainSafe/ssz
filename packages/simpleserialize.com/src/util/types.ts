e6acfe4b82913b51a64687d8a541d9444bc3cf69
Cayman <caymannava@gmail.com>
2022-02-23 11:11:45 -0600

    Update simpleserialize.com w bellatrix types (#237)
    Update lodestar-types to a version w bellatrix ns

--- packages/simpleserialize.com/src/util/types.ts
+++ packages/simpleserialize.com/src/util/types.ts
@@ -1,11 +1,12 @@
 import {Type} from "@chainsafe/ssz";
 import {ssz} from "@chainsafe/lodestar-types";
 
-const {phase0, altair, allForks, ...primitive} = ssz;
+const {phase0, altair, bellatrix, allForks, ...primitive} = ssz;
 
 export const forks = {
   phase0: {...phase0, ...primitive} as Record<string, Type<unknown>>,
   altair: {...phase0, ...altair, ...primitive} as Record<string, Type<unknown>>,
+  bellatrix: {...phase0, ...altair, ...bellatrix, ...primitive} as Record<string, Type<unknown>>,
 } as Record<string, Record<string, Type<unknown>>>
 
 export type ForkName = keyof typeof forks;
