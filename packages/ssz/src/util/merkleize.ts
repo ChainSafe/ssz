216b5a67b77e5227979f71af9e2a8af55ff9de5a
harkamal <gajinder@g11.in>
2022-04-12 21:57:23 +0530

    lint

--- packages/ssz/src/util/merkleize.ts
+++ packages/ssz/src/util/merkleize.ts
@@ -2,7 +2,7 @@ import {digest64Bytes32} from "@chainsafe/as-sha256";
 import {zeroHash} from "./zeros";
 
 export function hash64(bytes32A: Uint8Array, bytes32B: Uint8Array): Uint8Array {
-  return digest64Bytes32(bytes32A,bytes32B);
+  return digest64Bytes32(bytes32A, bytes32B);
 }
 
 export function merkleize(chunks: Uint8Array[], padFor: number): Uint8Array {
