21c8fdaf12b1e3a3b9499f6b429a5e4d95152108
Cayman <caymannava@gmail.com>
2023-03-28 16:08:52 -0400

    Add some comments

--- packages/persistent-merkle-tree/src/hasher/index.ts
+++ packages/persistent-merkle-tree/src/hasher/index.ts
@@ -5,8 +5,16 @@ export {HashObject} from "@chainsafe/as-sha256/hashObject";
 export * from "./types";
 export * from "./util";
 
+/**
+ * Hasher used across the SSZ codebase
+ */
 export let hasher: Hasher = nobleHasher;
 
+/**
+ * Set the hasher to be used across the SSZ codebase
+ *
+ * WARNING: This function is intended for power users and must be executed before any other SSZ code is imported
+ */
 export function setHasher(newHasher: Hasher): void {
   hasher = newHasher;
 }
--- packages/persistent-merkle-tree/src/hasher/types.ts
+++ packages/persistent-merkle-tree/src/hasher/types.ts
@@ -1,6 +1,12 @@
-import {HashObject} from "@chainsafe/as-sha256/hashObject";
+import type {HashObject} from "@chainsafe/as-sha256/hashObject";
 
 export type Hasher = {
+  /**
+   * Hash two 32-byte Uint8Arrays
+   */
   digest64(a32Bytes: Uint8Array, b32Bytes: Uint8Array): Uint8Array;
+  /**
+   * Hash two 32-byte HashObjects
+   */
   digest64HashObjects(a: HashObject, b: HashObject): HashObject;
 };
