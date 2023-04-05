4b44614003619b2c5477363a3c85287e2f2987bd
Cayman <caymannava@gmail.com>
2023-04-05 09:48:28 -0400

    feat: add swappable hasher, default to noble-hashes (#314)
    * Add swappable hasher, default to noble-hashes
    
    * Use as-sha256 hasher in benchmarks
    
    * Add some comments
    
    * Use as-sha256 hasher in tests
    
    * Increase test timeout

---
+++ packages/persistent-merkle-tree/src/hasher/util.ts
@@ -0,0 +1,11 @@
+import {byteArrayToHashObject, HashObject, hashObjectToByteArray} from "@chainsafe/as-sha256/hashObject";
+
+export function hashObjectToUint8Array(obj: HashObject): Uint8Array {
+  const byteArr = new Uint8Array(32);
+  hashObjectToByteArray(obj, byteArr, 0);
+  return byteArr;
+}
+
+export function uint8ArrayToHashObject(byteArr: Uint8Array): HashObject {
+  return byteArrayToHashObject(byteArr);
+}
