432f30d88b06a238e0eaa461c751bde735ca9c27
Tuyen Nguyen <vutuyen2636@gmail.com>
2024-04-15 15:20:40 +0700

    chore: documentation and benchmark in CI

--- packages/as-sha256/test/perf/simd.test.ts
+++ packages/as-sha256/test/perf/simd.test.ts
@@ -2,6 +2,14 @@ import { itBench, setBenchOpts } from "@dapplion/benchmark";
 import * as sha256 from "../../src";
 import {byteArrayToHashObject} from "../../src/hashObject";
 
+/**
+ * Mac M1 Apr 2024
+ *
+  digest64 vs hash4Inputs vs hash8HashObjects
+    ✓ digest64 200092 times                                               6.816078 ops/s    146.7119 ms/op        -         66 runs   10.3 s
+    ✓ hash 200092 times using hash4Input64s                               8.093460 ops/s    123.5566 ms/op        -         78 runs   10.2 s
+    ✓ hash 200092 times using hash8HashObjects                            8.141334 ops/s    122.8300 ms/op        -         78 runs   10.2 s
+ */
 describe("digest64 vs hash4Inputs vs hash8HashObjects", function () {
   this.timeout(0);
 
@@ -12,20 +20,23 @@ describe("digest64 vs hash4Inputs vs hash8HashObjects", function () {
   const input = Buffer.from("gajindergajindergajindergajindergajindergajindergajindergajinder", "utf8");
   // total number of time running hash for 200000 balances
   const iterations = 50023;
-  itBench(`digest64 ${iterations} times`, () => {
-    for (let j = 0; j < iterations; j++) sha256.digest64(input);
+  itBench(`digest64 ${iterations * 4} times`, () => {
+    for (let j = 0; j < iterations * 4; j++) sha256.digest64(input);
   });
 
   // hash4Inputs do 4 sha256 in parallel
-  const iterations2 = Math.floor(iterations / 4);
-  itBench(`hash ${iterations * 4} times using hash4Inputs`, () => {
-    for (let j = 0; j < iterations; j++) sha256.hash4Inputs(input, input, input, input);
+  itBench(`hash ${iterations * 4} times using hash4Input64s`, () => {
+    for (let j = 0; j < iterations; j++) {
+      sha256.hash4Input64s([input, input, input, input]);
+    }
   });
 
   const hashObject = byteArrayToHashObject(Buffer.from("gajindergajindergajindergajinder", "utf8"));
   const hashInputs = Array.from({length: 8}, () => hashObject);
   // hash8HashObjects do 4 sha256 in parallel
   itBench(`hash ${iterations * 4} times using hash8HashObjects`, () => {
-    for (let j = 0; j < iterations; j++) sha256.hash8HashObjects(hashInputs);
+    for (let j = 0; j < iterations; j++) {
+      sha256.hash8HashObjects(hashInputs);
+    }
   });
 });
--- packages/as-sha256/test/unit/simd.test.ts
+++ packages/as-sha256/test/unit/simd.test.ts
@@ -8,7 +8,7 @@ describe("Test SIMD implementation of as-sha256", () => {
     const input1 = "gajindergajindergajindergajinder";
     const input2 = "gajindergajindergajindergajinder";
     const input = Buffer.from(input1 + input2, "utf8");
-    const outputs = sha256.hash4Inputs(input, input, input, input);
+    const outputs = sha256.hash4Input64s([input, input, input, input]);
     const expectedOutput = new Uint8Array([
       190, 57, 56, 15, 241, 208, 38, 30, 111, 55, 218, 254, 66, 120, 182, 98, 239, 97, 31, 28, 178, 247, 192, 161,
       131, 72, 178, 215, 235, 20, 207, 110,
@@ -21,7 +21,7 @@ describe("Test SIMD implementation of as-sha256", () => {
   it("testHash4Inputs 1000 times", () => {
     for (let i = 0; i < 1000; i++) {
       const input = crypto.randomBytes(64);
-      const outputs = sha256.hash4Inputs(input, input, input, input);
+      const outputs = sha256.hash4Input64s([input, input, input, input]);
       const expectedOutput = sha256.digest64(input);
       expect(outputs[0]).to.be.deep.equal(expectedOutput);
       expect(outputs[1]).to.be.deep.equal(expectedOutput);
