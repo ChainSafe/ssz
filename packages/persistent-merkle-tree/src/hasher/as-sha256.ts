102de19f32c05ad3b65c75c63c3c9988758d5164
Tuyen Nguyen <vutuyen2636@gmail.com>
2024-05-28 14:58:52 +0900

    chore: rename variables

--- packages/persistent-merkle-tree/src/hasher/as-sha256.ts
+++ packages/persistent-merkle-tree/src/hasher/as-sha256.ts
@@ -38,16 +38,16 @@ export const hasher: Hasher = {
       // HashComputations of the same level are safe to batch
       let src0_0: Node | null = null;
       let src1_0: Node | null = null;
-      let dest_0: Node | null = null;
+      let dest0: Node | null = null;
       let src0_1: Node | null = null;
       let src1_1: Node | null = null;
-      let dest_1: Node | null = null;
+      let dest1: Node | null = null;
       let src0_2: Node | null = null;
       let src1_2: Node | null = null;
-      let dest_2: Node | null = null;
+      let dest2: Node | null = null;
       let src0_3: Node | null = null;
       let src1_3: Node | null = null;
-      let dest_3: Node | null = null;
+      let dest3: Node | null = null;
 
       for (const [i, hc] of hcArr.entries()) {
         const indexInBatch = i % 4;
@@ -55,22 +55,22 @@ export const hasher: Hasher = {
           case 0:
             src0_0 = hc.src0;
             src1_0 = hc.src1;
-            dest_0 = hc.dest;
+            dest0 = hc.dest;
             break;
           case 1:
             src0_1 = hc.src0;
             src1_1 = hc.src1;
-            dest_1 = hc.dest;
+            dest1 = hc.dest;
             break;
           case 2:
             src0_2 = hc.src0;
             src1_2 = hc.src1;
-            dest_2 = hc.dest;
+            dest2 = hc.dest;
             break;
           case 3:
             src0_3 = hc.src0;
             src1_3 = hc.src1;
-            dest_3 = hc.dest;
+            dest3 = hc.dest;
             break;
           default:
             throw Error(`Unexpected indexInBatch ${indexInBatch}`);
@@ -80,16 +80,16 @@ export const hasher: Hasher = {
           indexInBatch === 3 &&
           src0_0 !== null &&
           src1_0 !== null &&
-          dest_0 !== null &&
+          dest0 !== null &&
           src0_1 !== null &&
           src1_1 !== null &&
-          dest_1 !== null &&
+          dest1 !== null &&
           src0_2 !== null &&
           src1_2 !== null &&
-          dest_2 !== null &&
+          dest2 !== null &&
           src0_3 !== null &&
           src1_3 !== null &&
-          dest_3 !== null
+          dest3 !== null
         ) {
           const [o0, o1, o2, o3] = batchHash4HashObjectInputs([
             src0_0,
@@ -102,40 +102,40 @@ export const hasher: Hasher = {
             src1_3,
           ]);
           if (o0 == null || o1 == null || o2 == null || o3 == null) {
-            throw Error(`batchHash4HashObjectInputs return null at batch ${i} level ${level}`);
+            throw Error(`batchHash4HashObjectInputs return null or undefined at batch ${i} level ${level}`);
           }
-          dest_0.applyHash(o0);
-          dest_1.applyHash(o1);
-          dest_2.applyHash(o2);
-          dest_3.applyHash(o3);
+          dest0.applyHash(o0);
+          dest1.applyHash(o1);
+          dest2.applyHash(o2);
+          dest3.applyHash(o3);
 
           src0_0 = null;
           src1_0 = null;
-          dest_0 = null;
+          dest0 = null;
           src0_1 = null;
           src1_1 = null;
-          dest_1 = null;
+          dest1 = null;
           src0_2 = null;
           src1_2 = null;
-          dest_2 = null;
+          dest2 = null;
           src0_3 = null;
           src1_3 = null;
-          dest_3 = null;
+          dest3 = null;
         }
       }
 
       // remaining
-      if (src0_0 !== null && src1_0 !== null && dest_0 !== null) {
-        dest_0.applyHash(digest64HashObjects(src0_0, src1_0));
+      if (src0_0 !== null && src1_0 !== null && dest0 !== null) {
+        dest0.applyHash(digest64HashObjects(src0_0, src1_0));
       }
-      if (src0_1 !== null && src1_1 !== null && dest_1 !== null) {
-        dest_1.applyHash(digest64HashObjects(src0_1, src1_1));
+      if (src0_1 !== null && src1_1 !== null && dest1 !== null) {
+        dest1.applyHash(digest64HashObjects(src0_1, src1_1));
       }
-      if (src0_2 !== null && src1_2 !== null && dest_2 !== null) {
-        dest_2.applyHash(digest64HashObjects(src0_2, src1_2));
+      if (src0_2 !== null && src1_2 !== null && dest2 !== null) {
+        dest2.applyHash(digest64HashObjects(src0_2, src1_2));
       }
-      if (src0_3 !== null && src1_3 !== null && dest_3 !== null) {
-        dest_3.applyHash(digest64HashObjects(src0_3, src1_3));
+      if (src0_3 !== null && src1_3 !== null && dest3 !== null) {
+        dest3.applyHash(digest64HashObjects(src0_3, src1_3));
       }
     }
   },
