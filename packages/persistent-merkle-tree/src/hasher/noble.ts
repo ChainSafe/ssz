34de5e582764bc2ff9c7033aa8df1db2837689c5
matthewkeil <me@matthewkeil.com>
2024-06-10 19:33:06 +0200

    feat: add name to hasher for type detection in tests

--- packages/persistent-merkle-tree/src/hasher/noble.ts
+++ packages/persistent-merkle-tree/src/hasher/noble.ts
@@ -6,6 +6,7 @@ import {hashObjectToUint8Array, uint8ArrayToHashObject} from "./util";
 const digest64 = (a: Uint8Array, b: Uint8Array): Uint8Array => sha256.create().update(a).update(b).digest();
 
 export const hasher: Hasher = {
+  name: "noble",
   digest64,
   digest64HashObjects: (a, b) => uint8ArrayToHashObject(digest64(hashObjectToUint8Array(a), hashObjectToUint8Array(b))),
   batchHashObjects: (inputs: HashObject[]) => {
