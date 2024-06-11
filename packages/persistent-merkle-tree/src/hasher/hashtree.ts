037715bfcc87b1e4c87a5c78000b70c8575f3c12
matthewkeil <me@matthewkeil.com>
2024-06-11 11:32:42 +0200

    feat: improve hashtree performance a bit

--- packages/persistent-merkle-tree/src/hasher/hashtree.ts
+++ packages/persistent-merkle-tree/src/hasher/hashtree.ts
@@ -6,11 +6,11 @@ import {HashComputation, Node} from "../node";
 export const hasher: Hasher = {
   name: "hashtree",
   digest64(obj1: Uint8Array, obj2: Uint8Array): Uint8Array {
-    return hash(Uint8Array.from([...obj1, ...obj2]));
+    return hash(Buffer.concat([obj1, obj2], 64));
   },
   digest64HashObjects(obj1: HashObject, obj2: HashObject): HashObject {
-    const input1 = Uint8Array.from(new Array<number>(32));
-    const input2 = Uint8Array.from(new Array<number>(32));
+    const input1 = new Uint8Array(32);
+    const input2 = new Uint8Array(32);
     hashObjectToByteArray(obj1, input1, 0);
     hashObjectToByteArray(obj2, input2, 0);
     return byteArrayToHashObject(hasher.digest64(input1, input2));
@@ -21,9 +21,9 @@ export const hasher: Hasher = {
       return [];
     }
     // size input array to 2 HashObject per computation * 32 bytes per object
-    const input: Uint8Array = Uint8Array.from(new Array(inputs.length * 32));
+    const input = new Uint8Array(inputs.length * 32);
     inputs.forEach((hashObject, i) => hashObjectToByteArray(hashObject, input, i * 32));
-    const result: Uint8Array = hash(input);
+    const result = hash(input);
     const outputs: HashObject[] = [];
     for (let i = 0; i < inputs.length / 2; i++) {
       const offset = i * 32;
