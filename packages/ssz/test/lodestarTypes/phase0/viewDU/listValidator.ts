e08306b942201ad30e588403c5a5c238f169e4d5
Tuyen Nguyen <vutuyen2636@gmail.com>
2024-06-24 13:26:26 +0700

    fix: allocate memory once for ListValidatorTreeViewDU

--- packages/ssz/test/lodestarTypes/phase0/viewDU/listValidator.ts
+++ packages/ssz/test/lodestarTypes/phase0/viewDU/listValidator.ts
@@ -12,39 +12,33 @@ import { byteArrayToHashObject } from "@chainsafe/as-sha256";
  * Given a level3 of validators have 8 chunks, we can hash 4 validators at a time
  */
 const PARALLEL_FACTOR = 4;
+/**
+ * Allocate memory once for batch hash validators.
+ */
+// each level 3 of validator has 8 chunks, each chunk has 32 bytes
+const batchLevel3Bytes = new Uint8Array(PARALLEL_FACTOR * 8 * 32);
+const level3ByteViewsArr: ByteViews[] = [];
+for (let i = 0; i < PARALLEL_FACTOR; i++) {
+  const uint8Array = batchLevel3Bytes.subarray(i * 8 * 32, (i + 1) * 8 * 32);
+  const dataView = new DataView(uint8Array.buffer, uint8Array.byteOffset, uint8Array.byteLength);
+  level3ByteViewsArr.push({uint8Array, dataView});
+}
+const singleLevel3ByteView = level3ByteViewsArr[0];
+// each level 4 of validator has 2 chunks for pubkey, each chunk has 32 bytes
+const batchLevel4Bytes = new Uint8Array(PARALLEL_FACTOR * 2 * 32);
+const level4BytesArr: Uint8Array[] = [];
+for (let i = 0; i < PARALLEL_FACTOR; i++) {
+  level4BytesArr.push(batchLevel4Bytes.subarray(i * 2 * 32, (i + 1) * 2 * 32));
+}
+const singleLevel4Bytes = level4BytesArr[0];
 
 export class ListValidatorTreeViewDU extends ListCompositeTreeViewDU<ValidatorNodeStructType> {
-  private batchLevel3Bytes: Uint8Array;
-  private batchLevel4Bytes: Uint8Array;
-  // 32 * 8 = 256 bytes each
-  private level3ByteViewsArr: ByteViews[];
-  // 64 bytes each
-  private level4BytesArr: Uint8Array[];
-  private singleLevel3ByteView: ByteViews;
-  private singleLevel4Bytes: Uint8Array;
-
   constructor(
     readonly type: ListCompositeType<ValidatorNodeStructType>,
     protected _rootNode: Node,
     cache?: ArrayCompositeTreeViewDUCache
   ) {
     super(type, _rootNode, cache);
-    // each level 3 of validator has 8 chunks, each chunk has 32 bytes
-    this.batchLevel3Bytes = new Uint8Array(PARALLEL_FACTOR * 8 * 32);
-    this.level3ByteViewsArr = [];
-    for (let i = 0; i < PARALLEL_FACTOR; i++) {
-      const uint8Array = this.batchLevel3Bytes.subarray(i * 8 * 32, (i + 1) * 8 * 32);
-      const dataView = new DataView(uint8Array.buffer, uint8Array.byteOffset, uint8Array.byteLength);
-      this.level3ByteViewsArr.push({uint8Array, dataView});
-    }
-    this.singleLevel3ByteView = this.level3ByteViewsArr[0];
-    // each level 4 of validator has 2 chunks for pubkey, each chunk has 32 bytes
-    this.batchLevel4Bytes = new Uint8Array(PARALLEL_FACTOR * 2 * 32);
-    this.level4BytesArr = [];
-    for (let i = 0; i < PARALLEL_FACTOR; i++) {
-      this.level4BytesArr.push(this.batchLevel4Bytes.subarray(i * 2 * 32, (i + 1) * 2 * 32));
-    }
-    this.singleLevel4Bytes = this.level4BytesArr[0];
   }
 
   commit(hashComps: HashComputationGroup | null = null): void {
@@ -66,18 +60,18 @@ export class ListValidatorTreeViewDU extends ListCompositeTreeViewDU<ValidatorNo
       const indexInBatch = i % PARALLEL_FACTOR;
       const viewIndex = indicesChanged[i];
       const viewChanged = this.viewsChanged.get(viewIndex) as ValidatorTreeViewDU;
-      viewChanged.valueToMerkleBytes(this.level3ByteViewsArr[indexInBatch], this.level4BytesArr[indexInBatch]);
+      viewChanged.valueToMerkleBytes(level3ByteViewsArr[indexInBatch], level4BytesArr[indexInBatch]);
 
       if (indexInBatch === PARALLEL_FACTOR - 1) {
         // hash level 4
-        const pubkeyRoots = digestNLevelUnsafe(this.batchLevel4Bytes, 1);
+        const pubkeyRoots = digestNLevelUnsafe(batchLevel4Bytes, 1);
         if (pubkeyRoots.length !== PARALLEL_FACTOR * 32) {
           throw new Error(`Invalid pubkeyRoots length, expect ${PARALLEL_FACTOR * 32}, got ${pubkeyRoots.length}`);
         }
         for (let j = 0; j < PARALLEL_FACTOR; j++) {
-          this.level3ByteViewsArr[j].uint8Array.set(pubkeyRoots.subarray(j * 32, (j + 1) * 32), 0);
+          level3ByteViewsArr[j].uint8Array.set(pubkeyRoots.subarray(j * 32, (j + 1) * 32), 0);
         }
-        const validatorRoots = digestNLevelUnsafe(this.batchLevel3Bytes, 3);
+        const validatorRoots = digestNLevelUnsafe(batchLevel3Bytes, 3);
         if (validatorRoots.length !== PARALLEL_FACTOR * 32) {
           throw new Error(`Invalid validatorRoots length, expect ${PARALLEL_FACTOR * 32}, got ${validatorRoots.length}`);
         }
@@ -100,15 +94,15 @@ export class ListValidatorTreeViewDU extends ListCompositeTreeViewDU<ValidatorNo
     for (let i = endBatch; i < indicesChanged.length; i++) {
       const viewIndex = indicesChanged[i];
       const viewChanged = this.viewsChanged.get(viewIndex) as ValidatorTreeViewDU;
-      viewChanged.valueToMerkleBytes(this.singleLevel3ByteView, this.singleLevel4Bytes);
+      viewChanged.valueToMerkleBytes(singleLevel3ByteView, singleLevel4Bytes);
       // level 4 hash
-      const pubkeyRoot = digestNLevelUnsafe(this.singleLevel4Bytes, 1);
+      const pubkeyRoot = digestNLevelUnsafe(singleLevel4Bytes, 1);
       if (pubkeyRoot.length !== 32) {
         throw new Error(`Invalid pubkeyRoot length, expect 32, got ${pubkeyRoot.length}`);
       }
-      this.singleLevel3ByteView.uint8Array.set(pubkeyRoot, 0);
+      singleLevel3ByteView.uint8Array.set(pubkeyRoot, 0);
       // level 3 hash
-      const validatorRoot = digestNLevelUnsafe(this.singleLevel3ByteView.uint8Array, 3);
+      const validatorRoot = digestNLevelUnsafe(singleLevel3ByteView.uint8Array, 3);
       if (validatorRoot.length !== 32) {
         throw new Error(`Invalid validatorRoot length, expect 32, got ${validatorRoot.length}`);
       }
