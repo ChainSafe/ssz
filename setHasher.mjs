158601ab77f19466014106969374024600aacc2b
Tuyen Nguyen <vutuyen2636@gmail.com>
2024-06-29 20:44:59 +0700

    fix: use hashtree in setHasher.mjs for test

--- setHasher.mjs
+++ setHasher.mjs
@@ -1,5 +1,5 @@
 // Set the hasher to hashtree
 // Used to run benchmarks with with visibility into hashtree performance, useful for Lodestar
 import {setHasher} from "@chainsafe/persistent-merkle-tree/lib/hasher/index.js";
-import {hasher} from "@chainsafe/persistent-merkle-tree/lib/hasher/as-sha256.js";
+import {hasher} from "@chainsafe/persistent-merkle-tree/lib/hasher/hashtree.js";
 setHasher(hasher);
