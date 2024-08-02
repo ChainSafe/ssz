ed54907e32ccd72e84ead3e68997dcc7c850b0e8
Tuyen Nguyen <vutuyen2636@gmail.com>
2024-08-03 06:12:00 +0700

    chore: add benchmark result

--- packages/persistent-merkle-tree/test/perf/hashComputation.test.ts
+++ packages/persistent-merkle-tree/test/perf/hashComputation.test.ts
@@ -1,12 +1,19 @@
 import {itBench} from "@dapplion/benchmark";
 import { HashComputation, HashComputationLevel, LeafNode, zeroHash } from "../../src";
 
+/**
+ * HashComputationLevel push then loop is faster than HashComputation[] push then loop
+ * This is on Mac M1:
+ * HashComputationLevel
+    ✓ HashComputationLevel.push then loop                                 58.75361 ops/s    17.02023 ms/op        -         19 runs  0.835 s
+    ✓ HashComputation[] push then loop                                    36.51973 ops/s    27.38246 ms/op        -        150 runs   4.63 s
+ */
 describe("HashComputationLevel", function () {
   const src = LeafNode.fromRoot(zeroHash(0));
   const dest = LeafNode.fromRoot(zeroHash(1));
   const hashComp: HashComputation = {src0: src, src1: src, dest, next: null};
 
-  const length = 1_000_000;
+  const length = 2_000_000;
 
   itBench({
     id: "HashComputationLevel.push then loop",
