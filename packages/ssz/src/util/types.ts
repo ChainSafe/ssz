bee151d08443500d7213712e8b71422de3e93c6d
Tuyen Nguyen <vutuyen2636@gmail.com>
2024-09-11 09:03:28 +0700

    chore: export types

--- packages/ssz/src/util/types.ts
+++ packages/ssz/src/util/types.ts
@@ -1,5 +1,6 @@
 export type Require<T, K extends keyof T> = T & Required<Pick<T, K>>;
 
+/** Snapshot for PartialListCompositeType */
 export type Snapshot = {
   finalized: Uint8Array[];
   root: Uint8Array;
