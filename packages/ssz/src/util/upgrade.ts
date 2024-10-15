a2743475c0d7d6a1ba5f51f31405c93b9d940e6c
Cayman <caymannava@gmail.com>
2024-10-15 16:52:17 -0400

    chore: upgradeToNewType as utility function

---
+++ packages/ssz/src/util/upgrade.ts
@@ -0,0 +1,25 @@
+import {BranchNode, Node, zeroNode} from "@chainsafe/persistent-merkle-tree";
+import {ContainerType} from "../type/container";
+
+/** Upgrade the current View/ViewDU to a root node of new type */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function upgradeToNewType(rootNode: Node, oldType: ContainerType<any>, newType: ContainerType<any>): Node {
+  const newFieldsCount = newType.fieldsEntries.length;
+  const currentFieldsCount = oldType.fieldsEntries.length;
+  if (newFieldsCount < currentFieldsCount) {
+    throw Error(`Cannot convert to a type with fewer fields: ${newFieldsCount} < ${currentFieldsCount}`);
+  }
+
+  if (newType.depth === oldType.depth) {
+    // no need to grow the tree
+    return rootNode;
+  }
+
+  // grow the tree
+  let node = rootNode;
+  for (let depth = oldType.depth; depth < newType.depth; depth++) {
+    node = new BranchNode(node, zeroNode(depth));
+  }
+
+  return node;
+}
