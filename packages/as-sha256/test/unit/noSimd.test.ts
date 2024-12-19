4e07997c889c469a3de47961a46b6b94987e1b29
matthewkeil <me@matthewkeil.com>
2024-12-20 06:14:55 +0700

    fix: trying to get non-simd to run during unit tests

--- packages/as-sha256/test/unit/noSimd.test.ts
+++ packages/as-sha256/test/unit/noSimd.test.ts
@@ -1,6 +1,6 @@
 import * as sha256 from "../../src/index.js";
 import {getSimdTests} from "./getSimdTests.js";
 
-const useSimd = true;
+const useSimd = false;
 sha256.reinitializeInstance(useSimd);
 getSimdTests(sha256, useSimd);
