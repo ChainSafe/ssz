09bba35b089cfc90177b67818bd775accd78919a
Nazar Hussain <nazarhussain@gmail.com>
2024-12-20 17:34:38 +0100

    Update hasher path

--- setHasher.mjs
+++ setHasher.mjs
@@ -1,6 +1,6 @@
 // Set the hasher to hashtree
 // Used to run benchmarks with with visibility into hashtree performance, useful for Lodestar
-import {setHasher} from "@chainsafe/persistent-merkle-tree";
+import {setHasher} from "@chainsafe/persistent-merkle-tree/lib/hasher/index.js";
 import {hasher} from "@chainsafe/persistent-merkle-tree/lib/hasher/hashtree.js";
 setHasher(hasher);
 
