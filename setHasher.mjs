3c084738d66fdeac2cfe8cdc4354e99ae603e55f
Nazar Hussain <nazarhussain@gmail.com>
2025-01-15 14:08:52 +0100

    Disable hasher to test

--- setHasher.mjs
+++ setHasher.mjs
@@ -2,6 +2,6 @@
 // Used to run benchmarks with with visibility into hashtree performance, useful for Lodestar
 import {setHasher} from "@chainsafe/persistent-merkle-tree/lib/cjs/hasher/index.js";
 import {hasher} from "@chainsafe/persistent-merkle-tree/lib/cjs/hasher/hashtree.js";
-setHasher(hasher);
+// setHasher(hasher);
 
 export {};
