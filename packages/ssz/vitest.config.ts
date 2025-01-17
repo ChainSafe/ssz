491dc2da9f3ead2ea9b0964f96b80a2e417779cb
Nazar Hussain <nazarhussain@gmail.com>
2025-01-17 17:09:41 +0100

    Update the test configuration

--- packages/ssz/vitest.config.ts
+++ packages/ssz/vitest.config.ts
@@ -1,4 +1,8 @@
 import {defineConfig, mergeConfig} from "vitest/config";
-import baseConfig from "../../vitest.base.unit.config";
+import baseConfig from "../../vitest.base.unit.config.js";
+import {getRuntime} from "../../vitest.base.unit.config.js";
 
-export default mergeConfig(baseConfig, defineConfig({}));
+export default mergeConfig(
+  baseConfig,
+  defineConfig({test: {coverage: {enabled: getRuntime() === "node", reporter: ["text", "clover"]}}})
+);
