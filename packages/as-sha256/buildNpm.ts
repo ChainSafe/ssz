238ef678c680c81edee518d937094c29402a946d
Nazar Hussain <nazarhussain@gmail.com>
2025-03-03 15:52:15 +0100

    Remove unused file

--- packages/as-sha256/buildNpm.ts
+++
@@ -1,7 +0,0 @@
-import {build, emptyDir} from "@deno/dnt";
-import packageJSON from "./package.json" with {type: "json"};
-import {getBuildOptions} from "../../scripts/buildNpm.ts";
-
-await emptyDir("./npm");
-
-await build(getBuildOptions(packageJSON));
